16e25ca4adea82d1e507726b8a1ed5917e5562a2
leminhhung2002 <96687620+leminhhung2002@users.noreply.github.com>
2022-08-12 21:29:03 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'PIG';
+const giftUrl = '';
+const eventName = 'Happy Birthday ';
+const titleCard = 'Tặng PIG ';
+const contentCard = ' Ngày 28/8 là một ngày thiên thân ra đời ,Thiên thần đó xin phép gọi là pig ❤️, Chúc PIG sinh nhật vui vẻ nhé,chúc PIG luôn luôn hạnh phúc ngày nào cũng như ngày sinh nhật,luôn luôn giỏi giang ,luôn luôn xinh gái ,lúc nào cũng nở nụ cười trên môi á , Spiderman luôn bảo vệ trái đất còn tôi sẽ bảo về PIG vì PIG là thế giới của tôi hì hì ,luôn luôn nở nụ cười nha cho dù có chuyện gì á vì có spiderman ở đây rùi  và  đặc biệt lúc nào cũng đáng yêu như bài nhạc này nha hihi ❤️ ';
+
+const giftImage = 'lan.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
